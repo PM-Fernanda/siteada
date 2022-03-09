5ce8205d0aef738ebed51675d6bb33b4f0dde1fb
Fernanda-Tech <95706562+SSF-Tech@users.noreply.github.com>
2022-03-09 16:58:17 +0000

    Acrescentei a linguagem Js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+if(document.getElementByID("nome").value != "" && document.getElementByID("email").value != ""){
+  alert("Prontinho! você receberá as novidades por email.")
+}else{
+  alert("Por favor, preencha os campos nome e email")
+}
